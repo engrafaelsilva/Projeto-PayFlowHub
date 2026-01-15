eb223edc8b0d3ac2801917ee8c2be9e6041305ee
Rafael Silva <rafaelp_silva01@hotmail.com>
2026-01-15 18:44:19 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/projetopessoal/payflowhub/PayflowhubApplication.java
@@ -0,0 +1,13 @@
+package br.projetopessoal.payflowhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayflowhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayflowhubApplication.class, args);
+    }
+
+}
